530d438b77a79cc165a9bec8589d8961a507b850
Kriz1200 <69976499+Kriz-Kov@users.noreply.github.com>
2022-05-01 18:20:13 +0100

    Pylint Corrections
    Updating the functions.py file to fix a couple of pylint errors, nothing major.

--- utils/functions.py
+++ utils/functions.py
@@ -1,6 +1,3 @@
-import os
-from math import ceil
-
 import requests
 from bs4 import BeautifulSoup
 
@@ -16,7 +13,7 @@ def download_avatar(url):
 
 
 def get_price(url):
-    try:        
+    try:
         header = {
             "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36",
             'referer': 'https://www.google.com/'
