ed5ba86f51a30225c8f5f759435ad0bd25934220
Kriz1200 <69976499+Kriz-Kov@users.noreply.github.com>
2022-05-01 18:15:50 +0100

    Updating config and statuses
    Added a lot more customisation to the config.json file, specifically for changing the bots status between the main types, may in the future add commands that the bot owner can perform to change the statuses dynamically but we'll see.

--- init.py
+++ init.py
@@ -16,11 +16,13 @@ with open("config.json") as file:
     creators = data["bot"]["creator"]
     token = data["bot"]["token"]
     owner = data["bot"]["owner"]
+    streaming = data["bot"]["status"]["streaming"]["boolean"]
+    watching = data["bot"]["status"]["watching"]["boolean"]
+    playing = data["bot"]["status"]["playing"]["boolean"]
 
 class Greetings(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
-        self._last_member = None
 
 @client.event
 async def on_ready():
@@ -47,7 +49,28 @@ async def on_ready():
           f" 「 {bot_version} 」|「 Created by: {creators} 」" + Fore.RESET)
     # print only the version number that i am using
     print(Fore.LIGHTYELLOW_EX + f" 「 d.py: {discord.__version__}  」|「 py: {platform.python_version()} 」" + Fore.RESET)
-    await client.change_presence(activity=discord.Streaming(platform= "Twitch", name='brokeback mountain', url='https://www.twitch.tv/monstercat'))
+    status = data["bot"]["status"]["status"]
+    if streaming == "True":
+        platform_ = data["bot"]["status"]["streaming"]["type"]
+        name = data["bot"]["status"]["streaming"]["game"]
+        url = data["bot"]["status"]["streaming"]["url"]
+        await client.change_presence(activity=discord.Streaming(platform=platform_, name=name, url=url))
+    elif watching == "True":
+        activity = data["bot"]["status"]["watching"]["watching"]
+        if status == "online":
+            await client.change_presence(status=discord.Status.online, activity=discord.Activity(type=discord.ActivityType.watching, name=activity))
+        elif status == "dnd":
+            await client.change_presence(status=discord.Status.dnd, activity=discord.Activity(type=discord.ActivityType.watching, name=activity))
+        elif status == "idle":
+            await client.change_presence(status=discord.Status.idle, activity=discord.Activity(type=discord.ActivityType.watching, name=activity))
+    elif playing == "True":
+        game = data["bot"]["status"]["playing"]["game"]
+        if status == "online":
+            await client.change_presence(status=discord.Status.online, activity=discord.Activity(type=discord.ActivityType.playing, name=game))
+        elif status == "dnd":
+            await client.change_presence(status=discord.Status.dnd, activity=discord.Activity(type=discord.ActivityType.playing, name=game))
+        elif status == "idle":
+            await client.change_presence(status=discord.Status.idle, activity=discord.Activity(type=discord.ActivityType.playing, name=game))
 
 
 # reload all cogs if the bot owner runs the command
