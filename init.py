6018f965943eaa418c7034ed3959992164ed9d42
Pa-per <69976499+Pa-per@users.noreply.github.com>
2022-05-07 00:37:40 +0100

    Refactoring/Cleanup

--- init.py
+++ init.py
@@ -5,10 +5,11 @@ import platform
 import discord
 from colorama import Fore
 from discord.ext import commands
+from pymongo import MongoClient
+
+from utils.functions import load_cogs, set_status
 
 intents = discord.Intents.all()
-
-client = commands.Bot(command_prefix="#", intents=intents)
 
 with open("config.json") as file:
     data = json.load(file)
@@ -16,50 +17,110 @@ with open("config.json") as file:
     creators = data["bot"]["creator"]
     token = data["bot"]["token"]
     owner = data["bot"]["owner"]
-    streaming = data["bot"]["status"]["streaming"]["boolean"]
-    watching = data["bot"]["status"]["watching"]["boolean"]
-    playing = data["bot"]["status"]["playing"]["boolean"]
+    cluster = MongoClient(data["mongodb"]["cluster"])
+    db = cluster["Akkadian"]
+    serverConfigs = db["Guilds"]
+
+
+async def get_prefix(self, message):
+
+    query = {"_id": message.guild.id}
+
+    guilds = serverConfigs.find(query)
+
+    for result in guilds:
+
+        return result["prefix"]
+
+client = commands.Bot(command_prefix=get_prefix, intents=intents)
+
 
 class Greetings(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
+
 @client.event
 async def on_ready():
-    print(str(os.listdir))
-    for filename in os.listdir("commands"):
-        if filename.endswith(".py"):
-            await client.load_extension(f"commands.{filename[:-3]}")
-            print(f"Loaded {filename[:-3]}")
-    for filename in os.listdir("events"):
-        if filename.endswith(".py"):
-            await client.load_extension(f"events.{filename[:-3]}")
-            print(f"Loaded {filename[:-3]}")
+
+    await load_cogs(client)
+
+    await set_status(client)
+
     print(Fore.CYAN +
           f" 「 {bot_version} 」|「 Created by: {creators} 」" + Fore.RESET)
+
     print(Fore.LIGHTYELLOW_EX +
           f" 「 d.py: {discord.__version__}  」|「 py: {platform.python_version()} 」" + Fore.RESET)
-    status = data["bot"]["status"]["status"]
-    if streaming == "True":
-        platform_ = data["bot"]["status"]["streaming"]["type"]
-        name = data["bot"]["status"]["streaming"]["game"]
-        url = data["bot"]["status"]["streaming"]["url"]
-        await client.change_presence(activity=discord.Streaming(platform=platform_, name=name, url=url))
-    elif watching == "True":
-        activity = data["bot"]["status"]["watching"]["watching"]
-        if status == "online":
-            await client.change_presence(status=discord.Status.online, activity=discord.Activity(type=discord.ActivityType.watching, name=activity))
-        elif status == "dnd":
-            await client.change_presence(status=discord.Status.dnd, activity=discord.Activity(type=discord.ActivityType.watching, name=activity))
-        elif status == "idle":
-            await client.change_presence(status=discord.Status.idle, activity=discord.Activity(type=discord.ActivityType.watching, name=activity))
-    elif playing == "True":
-        game = data["bot"]["status"]["playing"]["game"]
-        if status == "online":
-            await client.change_presence(status=discord.Status.online, activity=discord.Activity(type=discord.ActivityType.playing, name=game))
-        elif status == "dnd":
-            await client.change_presence(status=discord.Status.dnd, activity=discord.Activity(type=discord.ActivityType.playing, name=game))
-        elif status == "idle":
-            await client.change_presence(status=discord.Status.idle, activity=discord.Activity(type=discord.ActivityType.playing, name=game))
+
+
+@client.event
+async def on_guild_join(guild: discord.Guild):
+    query = {"_id": guild.id}
+
+    if serverConfigs.count_documents(query) == 0:
+
+        default_prefix = "."
+
+        new_prefix = {"_id": guild.id, "prefix": default_prefix}
+
+        serverConfigs.insert_one(new_prefix)
+
+
+@client.event
+async def on_guild_remove(guild: discord.Guild):
+    query = {"_id": guild.id}
+
+    if serverConfigs.count_documents(query) == 1:
+
+        serverConfigs.delete_one(query)
+
+        print("Removed guild from database")
+
+
+@client.event
+async def on_message(message):
+
+    if not message.guild:
+
+        return
+
+    else:
+
+        if message.content.startswith(
+            f"<@!{client.user.id}>"
+        ) or message.content.startswith(f"<@{client.user.id}>"):
+
+            query = {"_id": message.guild.id}
+
+            if serverConfigs.count_documents(query) == 0:
+
+                default_prefix = "-"
+
+                new_prefix = {"_id": message.guild.id,
+                              "prefix": default_prefix}
+
+                serverConfigs.insert_one(new_prefix)
+
+                await message.reply(
+                    f"**Hello!**\nMy prefix for this server is: `{default_prefix}`"
+                )
+
+            else:
+
+                query = {"_id": message.guild.id}
+
+                guild = serverConfigs.find(query)
+
+                for result in guild:
+
+                    prefix = result["prefix"]
+
+                    await message.reply(
+                        f"**Hello!**\nMy prefix for this server is: `{prefix}`"
+                    )
+
+        await client.process_commands(message)
+
 
 client.run(token)
--- utils/functions.py
+++ utils/functions.py
@@ -1,6 +1,53 @@
+import json
+import os
+
 import aiohttp
+import discord
 import requests
 from bs4 import BeautifulSoup
+
+with open("config.json") as file:
+    data = json.load(file)
+    streaming = data["bot"]["status"]["streaming"]["boolean"]
+    watching = data["bot"]["status"]["watching"]["boolean"]
+    playing = data["bot"]["status"]["playing"]["boolean"]
+
+
+async def load_cogs(client):
+    for filename in os.listdir("commands"):
+        if filename.endswith(".py"):
+            await client.load_extension(f"commands.{filename[:-3]}")
+            print(f"Loaded {filename[:-3]}")
+    for filename in os.listdir("events"):
+        if filename.endswith(".py"):
+            await client.load_extension(f"events.{filename[:-3]}")
+            print(f"Loaded {filename[:-3]}")
+
+
+async def set_status(client):
+    status = data["bot"]["status"]["status"]
+    if streaming == "True":
+        platform_ = data["bot"]["status"]["streaming"]["type"]
+        name = data["bot"]["status"]["streaming"]["game"]
+        url = data["bot"]["status"]["streaming"]["url"]
+        await client.change_presence(activity=discord.Streaming(platform=platform_, name=name, url=url))
+    elif watching == "True":
+        activity = data["bot"]["status"]["watching"]["watching"]
+        if status == "online":
+            await client.change_presence(status=discord.Status.online, activity=discord.Activity(type=discord.ActivityType.watching, name=activity))
+        elif status == "dnd":
+            await client.change_presence(status=discord.Status.dnd, activity=discord.Activity(type=discord.ActivityType.watching, name=activity))
+        elif status == "idle":
+            await client.change_presence(status=discord.Status.idle, activity=discord.Activity(type=discord.ActivityType.watching, name=activity))
+    elif playing == "True":
+        game = data["bot"]["status"]["playing"]["game"]
+        if status == "online":
+            await client.change_presence(status=discord.Status.online, activity=discord.Activity(type=discord.ActivityType.playing, name=game))
+        elif status == "dnd":
+            await client.change_presence(status=discord.Status.dnd, activity=discord.Activity(type=discord.ActivityType.playing, name=game))
+        elif status == "idle":
+            await client.change_presence(status=discord.Status.idle, activity=discord.Activity(type=discord.ActivityType.playing, name=game))
+
 
 def download_avatar(url):
     r = requests.get(url)
@@ -27,6 +74,7 @@ def get_price(url):
         return price_text, product_name
     except Exception as e:
         print(e)
+
 
 async def check_exists(name):
     session = aiohttp.ClientSession()
